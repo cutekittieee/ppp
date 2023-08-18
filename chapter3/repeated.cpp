2917d776cd5364cbed2ee3ab2860ad82a25706c4
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2023-08-18 15:52:54 +0200

    Create repeated.cpp

---
+++ chapter3/repeated.cpp
@@ -0,0 +1,14 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	string previous = " ";	// previous word; initialized to "not a word"
+	string current;			// current word
+
+	while (cin >> current)	// read a stream of words
+	{
+		if (previous == current)
+			cout << "repeated word: " << current << '\n';
+		previous = current;
+	}
+}
