8d9c2480fe15673e93b026105596a50c1d7f0776
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2023-08-20 17:08:15 +0200

    Create unsafe_conv.cpp

---
+++ chapter3/unsafe_conv.cpp
@@ -0,0 +1,13 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	int a = 20000;
+	char c = a;		// try to squeeze a large int into a small char
+	int b = c;
+
+	if (a != b)		// != means "not equal"
+		cout << "Ooops!: " << a << "!=" << b << '\n';
+	else
+		cout << "Wow! We have large characters\n";
+}
