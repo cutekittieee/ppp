9395d4cd379d9fc0476e6ac1bde10dfe1752a52c
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2023-08-25 14:40:48 +0200

    Create 3.cpp

---
+++ chapter3/drills/3.cpp
@@ -0,0 +1,19 @@
+// read and write a first name
+
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Enter the name of the person you want to write to:\n";
+	string first_name;
+	string friend_name;
+	cin >> first_name;
+	cout << "Dear " << first_name << ",\n";
+	cout << "\tHow are you? I am fine. I miss you. We have so many memories together we will never forget!\n";
+	cout << "I was very worried since you mentioned that a member of your family has become ill.\n";
+	cout << "Are they getting better? When are you going to return home to be able to visit them?\n";
+	cin >> friend_name;
+	cout << "Have you seen " << friend_name << " lately?\n";
+
+	return 0;
+}
