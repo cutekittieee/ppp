f4b89feea2f8cef537d89b77b02d90d94dd6edb1
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2023-08-30 14:27:20 +0200

    Create 4.cpp

---
+++ chapter3/exercises/4.cpp
@@ -0,0 +1,30 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	int val1, val2;
+
+	cout << "Please enter two integer values: ";
+	cin >> val1 >> val2;
+
+	if (val1 < val2)
+	{
+		cout << val1 << " is smaller than " << val2 << ".\n";
+		cout << val2 << " is larger than " << val1 << ".\n";
+	}
+	else if (val1 > val2)
+	{
+		cout << val2 << " is smaller than " << val1 << ".\n";
+		cout << val1 << " is larger than " << val2 << ".\n";
+	}
+	else
+		cout << "The two numbers (" << val1 << ") are equal.\n";
+
+	cout << "Sum: " << val1 << " + " << val2 << " = " << val1 + val2 << "\n";
+	cout << "Difference: " << val1 << " - " << val2 << " = " << val1 - val2 << "\n";
+	cout << "Product: " << val1 << " * " << val2 << " = " << val1 * val2 << "\n";
+	cout << "Ratio: " << val1 << " / " << val2 << " = " << val1 / val2 << "\n";
+	cout << "Remainder: " << val1 << " % " << val2 << " = " << val1 % val2 << "\n";
+
+	return 0;
+}
